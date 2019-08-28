671473b593a693b22077f4e99975c8f10677eab0
ItzcoR <itzcoatlpv@gmail.com>
2019-08-28 14:05:20 -0500

    Agregada la GUI

--- HilosTextos.py
+++ HilosTextos.py
@@ -3,7 +3,8 @@ import time
 import glob
 import os
 import re
-import Tkinter
+import json
+from tkinter import *
 
 directorioLibros="/home/itzco/Documents/Redes2/Practica1/librosCrudos/"
 palabrasAContar={"regalo":0 , "corazón":0, "delicioso":0, "perdón":0, "carta":0, "poesía":0}
@@ -51,17 +52,27 @@ def wordCount(wordsToMatch, file, result, index):
     result[index]=mydict
 
 #GUI
-window = TK()
-window.geometry('720*1000')
-window.tittle("Practica 1 Redes")
-lbl = label(window, text = "Libro")
+window = Tk()
+window.geometry('720x1000')
+window.title("Practica 1 Redes")
+"""lbl = Label(window, text = "Libro")
+lbl.pack()#lbl.grid(row=0)
+topframe = Frame(window)
+topframe.pack()
+bottomframe = Frame(window)
+bottomframe.pack(side=BOTTOM)"""
 # Create new threads
 
 txtCounter = len(glob.glob1(directorioLibros,"*.txt"))
 print(txtCounter)
-thread=[None]*txtCounter
-results=[None]*txtCounter
+thread=[None]*txtCounter# Arreglo de Hilos
+results=[None]*txtCounter#En este arreglo guardaremos los diccionarios de cada texto
+"""txtLabels = [None]*txtCounter #Arreglo de labels para la GUI
+resLabels = [None]*txtCounter #Arreglo de lables donde mostraremos los resultados de cada libro"""
 txtFiles = []
+scrollbar = Scrollbar(window)
+scrollbar.pack(side=RIGHT, fill=Y) #scrollbar.grid(column=1,row=0, rowspan=94, sticky= N+S+E)
+myListGUI = Listbox(window, yscrollcommand = scrollbar.set)
 for file in glob.glob1(directorioLibros,"*.txt"):
     txtFiles.append(file)
 for i in range (0,txtCounter):
@@ -73,14 +84,31 @@ for i in range (0,txtCounter):
     #thread[i].join()
 for i in range (0,txtCounter):
     thread[i].join()
+    myListGUI.insert(END,txtFiles[i])
+    #txtLabels[i] = Label(window, text = txtFiles[i], justify=RIGHT)
+    #txtLabels[i].pack()#txtLabels[i].grid(row=i+2, sticky=W)
     print("Texto: ",txtFiles[i],"palabras:")
     print (results[i])
+    dictnAsString= json.dumps(results[i])
+    myListGUI.insert(END,dictnAsString)
+    #resLabels[i] = Label(window, text = dictnAsString, justify=RIGHT)
+    #resLabels[i].pack()#resLabels[i].grid(row=i+1, sticky=W)
     print("\n")
 print("El numero total de palabras es:",totaldePalabras )
 print("El conteo global de palabras es:",globalCount )
 for word in globalCount.keys():
     globalCount[word]=float(globalCount[word]/totaldePalabras)
 print("La probabilidad es de:", globalCount)
+myListGUI.insert(END,"El numero total de palabras es de:"+str(totaldePalabras))
+dictnAsString= json.dumps(globalCount)
+myListGUI.insert(END,"La probabilidad es de: "+dictnAsString)
+myListGUI.pack(side = LEFT, fill = BOTH, expand=True)
+scrollbar.config( command = myListGUI.yview)
+
+#print("\n\n")
+#print(results)
+#print(results[0]['poesía'])
+
 print ("Exiting Main Thread")
 
 window.mainloop()
